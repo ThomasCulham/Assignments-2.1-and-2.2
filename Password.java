e47cbe86cf5422254691e8feb8bf1709657d53f1
ThomasCulham <56049828+ThomasCulham@users.noreply.github.com>
2019-10-01 14:20:26 -0700

    Add files via upload

--- Password.java
+++ Password.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 class Password {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String password = new String("password");
+        String password = new String("dragon");
         boolean unlocked= false;
         while(unlocked==false){
             System.out.println("What is the password?");
             String Input = sc.nextLine();
             if(password.equals(Input)){
-                System.out.println("Access granted!! Go to this link to find my nudes:");
+                System.out.println("Access granted!! Go to this link to find my secrets:");
                 System.out.println("https://TomHub.com");
                 unlocked=true;
             }else{
